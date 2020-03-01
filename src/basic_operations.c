047a7b2fa4fc51b49360c3ebb946b1198ee51c02
Sion Roland <sroland@oa-k6.21-school.ru>
2020-03-01 17:01:33 +0300

    updated src and incl

--- src/basic_operations.c
+++ src/basic_operations.c
@@ -6,23 +6,13 @@
 /*   By: sroland <sroland@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 14:49:54 by sroland           #+#    #+#             */
-/*   Updated: 2020/03/01 16:04:26 by sroland          ###   ########.fr       */
+/*   Updated: 2020/03/01 17:01:25 by sroland          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int			add_room(r_list *rooms, r_list *new_room)
-{
-
-}
-
-int			add_edge(r_list *room_from, r_list *room_to)
-{
-
-}
-
-t_room		*find_room(char *name)
+t_room		*find_room(t_graph *world, char *name)
 {
 
 }
---
+++ src/parse_content.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_content.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sroland <sroland@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 16:47:33 by sroland           #+#    #+#             */
+/*   Updated: 2020/03/01 17:01:25 by sroland          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+int			get_ants(t_graph *world, char *line)
+{
+
+}
+
+int			get_next_room(t_graph *world, char *line)
+{
+
+}
+
+int			get_edges(t_graph *world, char *line)
+{
+	
+}
