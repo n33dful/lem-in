746c5595fa9e239f8bd29598dcd786c62a1f0f5b
Sion Roland <sroland@oa-e6.21-school.ru>
2020-03-04 21:29:26 +0300

    h file modified

--- incl/lem_in.h
+++ incl/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: sroland <sroland@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 20:19:13 by sroland           #+#    #+#             */
-/*   Updated: 2020/03/04 21:25:24 by sroland          ###   ########.fr       */
+/*   Updated: 2020/03/04 21:28:52 by sroland          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,5 +55,16 @@ int						add_room(t_graph *world, int flag, char *line);
 int						get_edges(t_graph *world, char *line);
 void					ft_world_print(t_graph *world);
 void					ft_graphdel(t_graph *graph);
+
+int						bfs_find_next_path(t_graph *world);
+void					change_flow(int diff, t_room *from, t_room *to);
+int						bfs_travers(t_graph *world);
+t_room					*room_out(t_room *room);
+t_list					*delete_first_room(t_list **queue);
+int						nulify_flow(t_graph *world);
+int						nulify_parents_and_is_visited(t_graph *world);
+int						ford_falkerson(t_graph *world);
+
+
 
 #endif
